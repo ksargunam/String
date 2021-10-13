042ecb69a0ab7eb9c3678ae2143c6ecb6bfefcfc
ksargunam <sargunam31595@gmail.com>
2021-10-13 05:32:05 -0700

    message

---
+++ src/org/str/Company.java
@@ -0,0 +1,22 @@
+package org.str;
+
+public class Company {
+	public static void main(String[] args) {
+		String s="Welcome To Java Class";
+		System.out.println(s);
+		int length = s.length();
+		System.out.println(length);
+		boolean empty = s.isEmpty();
+		System.out.println(empty);
+		char charAt = s.charAt(3);
+		System.out.println(charAt);
+		int indexOf = s.indexOf("e");
+		System.out.println(indexOf);
+		int lastIndexOf = s.lastIndexOf("e");
+		System.out.println(lastIndexOf);
+		int indexOf2 = s.indexOf("q");
+		System.out.println(indexOf2);
+		
+	}
+
+}
